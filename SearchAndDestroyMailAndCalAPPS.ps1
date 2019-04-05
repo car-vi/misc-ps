7b22fddaf79b2844a2fd74e321efd1e9ef3f3b2e
car-vi <c.vieyra@outlook.com>
2019-04-05 12:18:05 -0400

    Add files via upload

---
+++ SearchAndDestroyMailAndCalAPPS.ps1
@@ -0,0 +1,10 @@
+﻿
+$ChAPP = Get-AppxPackage *microsoft.windowscommunicationsapps*
+$RemAPP = Get-AppxPackage *microsoft.windowscommunicationsapps* | Remove-AppxPackage
+
+if([string]::IsNullOrEmpty($ChAPP)){
+    Write-Host "NULL";
+}else {
+    Write-Host "Not NULL";
+    $RemAPP
+}

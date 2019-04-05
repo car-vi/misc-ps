efdb6f5fec21428d8c6b23f7b52a1a115bf65f36
car-vi <c.vieyra@outlook.com>
2019-04-05 12:20:01 -0400

    Add files via upload

---
+++ sendmail.ps1
@@ -0,0 +1,12 @@
+﻿$From = "email@email"
+$To = "email@email"
+$Cc = "email@email"
+#$Attachment = "C:\users\Username\Documents\SomeTextFile.txt"
+$Subject = "Test"
+$Body = "Test"
+$SMTPServer = "smtp_server"
+$SMTPPort = "25"
+#-UseSsl
+#-Credential (Get-Credential)
+#–DeliveryNotificationOption OnSuccess
+Send-MailMessage -From $From -to $To -Cc $Cc -Subject $Subject -Body $Body -SmtpServer $SMTPServer -port $SMTPPort
